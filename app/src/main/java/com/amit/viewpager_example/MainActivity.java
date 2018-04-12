1cc75991af1a634e71c2818c0a96a7ed085da87b
AmitMetri <amit.e18@gmail.com>
2018-04-13 02:18:32 +0530

    Initial commit

---
+++ app/src/main/java/com/amit/viewpager_example/MainActivity.java
@@ -0,0 +1,45 @@
+package com.amit.viewpager_example;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity {
+
+    private static final int NUM_PAGES = 3;
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (ViewPager) findViewById(R.id.viewpager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+    }
+
+
+    class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter{
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new ScreenSlidePagerFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES ;
+        }
+    }
+}
